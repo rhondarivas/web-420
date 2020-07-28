242cb3bb79d67678de8cd8f6df830e01443bfb42
Rhonda Rivas <verano@Rhondas-Air.attlocal.net>
2020-07-28 19:27:46 -0400

    updates

--- api-gateway/controllers/authController.js
+++ api-gateway/controllers/authController.js
@@ -7,31 +7,22 @@
 ==============================================
 */
 var User = require('../models/user');
-// Register a new user on POST
-exports.user_register = function(req, res) {
- res.send('NOT IMPLEMENTED: User registration POST');
-};
-// Verify token on GET
-exports.user_token = function(req, res) {
- res.send('NOT IMPLEMENTED: User token lookup GET');
-};
-var User = require('../models/user');
 var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
-var config = require('../config');
+var config = require('../config')
 
-//registers a new user on POST
+
+// Register a new user on POST
 exports.user_register = function(req, res) {
+    var hashedPassword = bcrypt.hashSync(req.body.password, 8);
 
-  var hashedPassword = bcrypt.hashSync(req.body.password, 8);
+    var newUser = new User({
+      username: req.body.username,
+      password: hashedPassword,
+      email: req.body.email
+  });
 
-  var newUser = new user({
-    username: req.body.username,
-    psssword: hashedPassword,
-    email: req.body.email
-});
-
-User.add(newUser, (err, user) => {
+  User.add(newUser, (err, user) => {
   if (err)
     return res.status(500).send('There was a problem registering the user.');
   var token = jwt.sign({ id: user._id}, config.web.secret, {
@@ -42,22 +33,24 @@ User.add(newUser, (err, user) => {
     });
 
   };
+
 //verifies token on GET
 exports.user_token = function(req, res) {
-  var token = req.headers['x-access-token'];
+    var token = req.headers['x-access-token'];
 
-  if (!token) return res.status(401).send({ auth: false, message: 'No token provided'});
+    if (!token) return res.status(401).send({ auth: false, message: 'No token provided'});
 
-  jwt.verify(token, config.web.secret, function(err,decoded) {
-    if (err) return res.status(500).send({auth: false, message:'Failed to authenitcate token'});
+    jwt.verify(token, config.web.secret, function(err, decoded) {
+      if (err) return res.status(500).send({auth: false, message:'Failed to authenticate token'});
 
-    User.getById(decoded.id, function(err, user) {
-      if (err) return res.status(500).send('There was a problem finding the user');
+      User.getById(decoded.id, function(err, user) {
+        if (err) return res.status(500).send('There was a problem finding the user');
 
-      if (!user) return res.status(404).send('No user found');
+        if (!user) return res.status(404).send('No user found');
 
-      res.status(200).send(user);
+        res.status(200).send(user);
+
+      });
 
     });
-  });
 };
