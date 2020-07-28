933fc054b674bc3d167010eecd9ddbf7aca23dce
Rhonda Rivas <verano@Rhondas-Air.attlocal.net>
2020-07-28 00:19:41 -0400

    week 4 updates

--- api-gateway/controllers/authController.js
+++ api-gateway/controllers/authController.js
@@ -1,9 +1,9 @@
 /*============================================
 ; Title: rivas -assignment 2.3 authControll.js
 ; Author: Richard Krasso
-; Date: 13 July 2020
+; Date: 27 July 2020
 ; Modified By: Rhonda Rivas
-; Description: Demonstrates Controllers
+; Description: Demonstrates Controllers API's
 ==============================================
 */
 var User = require('../models/user');
@@ -14,4 +14,50 @@ exports.user_register = function(req, res) {
 // Verify token on GET
 exports.user_token = function(req, res) {
  res.send('NOT IMPLEMENTED: User token lookup GET');
+};
+var User = require('../models/user');
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
+var config = require('../config');
+
+//registers a new user on POST
+exports.user_register = function(req, res) {
+
+  var hashedPassword = bcrypt.hashSync(req.body.password, 8);
+
+  var newUser = new user({
+    username: req.body.username,
+    psssword: hashedPassword,
+    email: req.body.email
+});
+
+User.add(newUser, (err, user) => {
+  if (err)
+    return res.status(500).send('There was a problem registering the user.');
+  var token = jwt.sign({ id: user._id}, config.web.secret, {
+      expiresIn: 86400 //24hrs
+  });
+  res.status(200).send({ auth: true, token: token });
+
+    });
+
+  };
+//verifies token on GET
+exports.user_token = function(req, res) {
+  var token = req.headers['x-access-token'];
+
+  if (!token) return res.status(401).send({ auth: false, message: 'No token provided'});
+
+  jwt.verify(token, config.web.secret, function(err,decoded) {
+    if (err) return res.status(500).send({auth: false, message:'Failed to authenitcate token'});
+
+    User.getById(decoded.id, function(err, user) {
+      if (err) return res.status(500).send('There was a problem finding the user');
+
+      if (!user) return res.status(404).send('No user found');
+
+      res.status(200).send(user);
+
+    });
+  });
 };
--- api-gateway/models/user.js
+++ api-gateway/models/user.js
@@ -1,7 +1,7 @@
 /*============================================
 ; Title: rivas -assignment 2.3 user.js
 ; Author: Richard Krasso
-; Date: 13 July 2020
+; Date: 27 July 2020
 ; Modified By: Rhonda Rivas
 ; Description: Demonstrates Mongoose Models
 ==============================================
@@ -20,3 +20,12 @@ let UserSchema = new Schema({
 
 // Export the model to make it available publicly.
 module.exports = mongoose.model('User', UserSchema);
+//user.save is used to add new user in our database
+module.exports.add = (user, callback) => {
+  user.save(callback);
+};
+// geyByID function
+module.exports.getById = (id, callback) => {
+  var query = {_id: id};
+  UserSchema.findById(query, callback);
+;}
