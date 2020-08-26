8e1d15a83dcf283c7d8e8e3cb1c33f962a590175
Rhonda Rivas <verano@Rhondas-Air.attlocal.net>
2020-08-26 15:16:25 -0400

    week 8 updates

--- api-gateway/controllers/authController.js
+++ api-gateway/controllers/authController.js
@@ -10,66 +10,61 @@
 var User = require('../models/user');
 var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
-var config = require('../config')
+var config = require('../config');
 
-
-// Register a new user on POST
-exports.user_register = function(req, res) {
+//Register a new user on POST
+exports.user_register = function (req, res){
     var hashedPassword = bcrypt.hashSync(req.body.password, 8);
 
     var newUser = new User({
-      username: req.body.username,
-      password: hashedPassword,
-      email: req.body.email
-  });
-
-  User.add(newUser, (err, user) => {
-  if (err)
-    return res.status(500).send('There was a problem registering the user.');
-  var token = jwt.sign({ id: user._id}, config.web.secret, {
-      expiresIn: 86400 //24hrs
-  });
-  res.status(200).send({ auth: true, token: token });
-
+        username: req.body.username,
+        password: hashedPassword,
+        email: req.body.email
     });
 
-  };
+    User.add(newUser, (err, user) => {
+        if (err)
+            return res.status(500).send('There was a problem registering the user.');
 
-// Login as an existing user in on POST
+            var token = jwt.sign({id: user._id}, config.web.secret, {
+                expiresIn: 86400 //24hours
+            });
+
+        res.status(200).send({auth: true, token: token});
+    });
+};
+
+//Verify token on GET
+exports.user_token = function (req, res){
+    User.getById(req.userId, function(err, user) {
+            if (err) return res.status(500).send('There was a problem finding the user.');
+
+            if (!user) return res.status(404).send('No user found.');
+
+            res.status(200).send(user);
+    });
+};
+
+//handles user login requests
 exports.user_login = function(req, res) {
 
-  User.getOne(req.body.email, function(err, user){
-    if (err) return res.status(500).send("Error on server");
-    if (!user) return res.status(404).send("No user found.");
+    User.getOne(req.body.email, function(err, user) {
+        if(err) return res.status(500).send('Error on server.');
+        if(!user) return res.status(404).send('No user found.');
 
-    var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+        var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
 
-    if (!passwordIsValid) return res.status(401).send({auth: false, token: null});
+        if(!passwordIsValid) return res.status(401).send({auth: false, token: null});
 
-    var token = jwt.sign({ id: user._id}, config.web.secret, {
-      expiresIn: 86400 //expires in 24hours
-    });
-    res.status(200).send( {auth: true, token: token});
-  })
+        var token = jwt.sign({id: user._id}, config.web.secret, {
+            expiresIn: 86400 //24 hours
+        });
+
+        res.status(200).send({auth: true, token: true});
+    })
 };
 
-// Logout an existing user
-exports.user_logout = function(req,res) {
-  res.status(200).send({ auth: false, token: null});
+//handles user logout requests
+exports.user_logout = function(req, res) {
+    res.status(200).send({auth: false, token: null});
 };
-
-// Verify token on GET
-exports.user_token = function(req, res) {
-  User.getById(req.userId, function(err, user) {
-      if (err) return res.status(500).send('There was a problem finding the user.');
-
-      if (!user) return res.status(404).send('No user found.');
-
-      res.status(200).send(user);
-  });
-};
-
-
-
-
-
--- api-gateway/routes/api-catalog.js
+++ api-gateway/routes/api-catalog.js
@@ -6,33 +6,24 @@
 ; Description: Demonstrates routes
 ==============================================
 */
-/**
-* API Routes
-*/
-var express = require('express');
-var checkToken = requre('../check-token');
+
+//API Routes
+var express = require ('express');
+var checkToken = require('../check-token');
 var router = express.Router();
-
-
 var auth_controller = require('../controllers/authController');
 
 
-
-// POST request for registering a user
+//POST request for registering a user
 router.post('/auth/register', auth_controller.user_register);
 
+//GET request for verifying user tokens
+router.get('/auth/token', checkToken, auth_controller.user_token);
 
-
-// GET request for verifying user tokens
-router.get('/auth/token',  checkToken, auth_controller.user_token);
-
-
-// POST request for signing users in
+//allow user login requests
 router.post('/auth/login', auth_controller.user_login);
 
-
-// GET request for logging users out
+//allow user logout requests
 router.get('/auth/logout', auth_controller.user_logout);
-
 
 module.exports = router;
